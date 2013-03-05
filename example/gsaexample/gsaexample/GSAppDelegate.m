2f0c7079c850719080974e7edd2d219a747429ea
Bao Lei <bao.lei@hulu.com>
2013-03-05 00:24:53 -0800

    Added LICENSE.txt

--- example/gsaexample/gsaexample/GSAppDelegate.m
+++ example/gsaexample/gsaexample/GSAppDelegate.m
@@ -3,7 +3,7 @@
 //  gsaexample
 //
 //  Created by BaoLei on 1/16/13.
-//  Copyright (c) 2013 Hulu. All rights reserved.
+//  Copyright (c) 2013 Hulu. All rights reserved. See LICENSE.txt.
 //
 
 #import "GSAppDelegate.h"
